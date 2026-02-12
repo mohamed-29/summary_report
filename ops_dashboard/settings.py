a95da2e48b2bde7ff887ed3a095e36ef65c5c029
Mohamed Hassan <hassaninin333@gmail.com>
2026-02-12 14:14:37 +0200

    db

--- ops_dashboard/settings.py
+++ ops_dashboard/settings.py
@@ -80,12 +80,24 @@ WSGI_APPLICATION = 'ops_dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "operationsdb",       # your database name
+            "USER": "ivend",          # your postgres user
+            "PASSWORD": "Iv@123456",
+            "HOST": "127.0.0.1",      # or the server IP / hostname
+            "PORT": "5432",           # default postgres port
+        }
+    }
 
 
 # Password validation
