29f8ad40ccb2ea8b23592a8c49102d0379cf9a20
Mohamed Hassan <hassaninin333@gmail.com>
2026-02-12 14:56:39 +0200

    ok

--- ops_dashboard/urls.py
+++ ops_dashboard/urls.py
@@ -27,7 +27,9 @@ urlpatterns = [
 ]
 
 # Serve media files in development
+# Serve static and media files in development
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
