28e5b481ad98e37fce91aa85da9c41333547d5d7
mohamed <ivend@mobica.net>
2026-04-15 12:12:53 +0200

    fix

--- logistics/views.py
+++ logistics/views.py
@@ -16,6 +16,7 @@ from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 
 
